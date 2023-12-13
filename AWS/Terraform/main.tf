a66e2d7027805ad6ca31f60099847a4f6dbe6060
c7031390 <emanuele.goldin@uibk.ac.at>
2023-12-13 13:51:06 +0100

    Add timeout to Lambda function and create API Gateway modules

--- AWS/Terraform/main.tf
+++ AWS/Terraform/main.tf
@@ -19,3 +19,17 @@ module "sagemaker" {
   image = "763104351884.dkr.ecr.us-east-1.amazonaws.com/huggingface-pytorch-inference:1.13.1-transformers4.26.0-gpu-py39-cu117-ubuntu20.04"
   model_data_url = "s3://jumpstart-cache-prod-us-east-1/huggingface-infer/prepack/v1.0.0/infer-prepack-huggingface-summarization-distilbart-cnn-6-6.tar.gz"
 }
+
+module "api_gateway_us_east_1" {
+  source = "./api_gateway"
+  region = "us-east-1"
+  lambda_arn = module.lambda_us_east_1.lambda_arn
+  lambda_function_name = module.lambda_us_east_1.lambda_function_name
+}
+
+module "api_gateway_us_west_2" {
+  source = "./api_gateway"
+  region = "us-west-2"
+  lambda_arn = module.lambda_us_west_2.lambda_arn
+  lambda_function_name = module.lambda_us_west_2.lambda_function_name
+}
--- AWS/lambda_function/lambda.py
+++ AWS/lambda_function/lambda.py
@@ -1,17 +1,33 @@
 import json
 import dummy_module
+import os
+import io
+import boto3
+import json
+import csv
+
+ENDPOINT_NAME = "summarizer-endpoint"
+runtime= boto3.client('runtime.sagemaker', region_name='us-east-1') # We deploy a single model to us-east-1
 
 def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
+    data = json.loads(event['body'])
+    payload = data['input']
     
-    # Create a response with dummy data for testing
-    response = {
+    encoded_text = payload.encode("utf-8")
+
+    # Invoke the SageMaker endpoint
+    response = runtime.invoke_endpoint(EndpointName=ENDPOINT_NAME,
+                                       ContentType='application/x-text',
+                                       Body=encoded_text)
+
+    # Unpack response
+    responseBody = response['Body'].read()
+    result = json.loads(responseBody)
+
+    # Return the summary
+    summary = result['summary_text']
+    return {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps({
-            "message": dummy_module.dummy_function()
-        })
+        "headers": { "Content-Type": "application/json" },
+        "body": json.dumps({ "summary": summary })
     }
-    return response
