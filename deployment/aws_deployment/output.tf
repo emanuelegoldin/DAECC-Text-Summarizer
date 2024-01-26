1570a2d06496d863f85974f2a6f58efa19864eee
c7031390 <emanuele.goldin@uibk.ac.at>
2024-01-26 18:49:13 +0100

    Updated aws deployment8split, merge)

--- deployment/aws_deployment/output.tf
+++ deployment/aws_deployment/output.tf
@@ -2,22 +2,42 @@
 #                                       AWS Outputs                                          #
 ##############################################################################################
 
-output "lambda_arn_us_west_2" {
+output "summarise_arn_us_west_2" {
   description = "The ARN of the Lambda function"
-  value       = module.lambda_us_west_2.lambda_arn
+  value       = module.lambda_us_west_2.summarise_arn
 }
 
-output "lambda_arn_us_east_1" {
+output "summarise_arn_us_east_1" {
   description = "The ARN of the Lambda function"
-  value       = module.lambda_us_east_1.lambda_arn
+  value       = module.lambda_us_east_1.summarise_arn
 }
 
-output "api_gateway_url_us_west_2" {
-  description = "The URL of the API Gateway"
-  value       = module.api_gateway_us_west_2.api_gateway_url
+output "split_arn_us_west_2" {
+  description = "The ARN of the Lambda function"
+  value       = module.lambda_us_west_2.split_arn
 }
 
-output "api_gateway_url_us_east_1" {
-  description = "The URL of the API Gateway"
-  value       = module.api_gateway_us_east_1.api_gateway_url
+output "split_arn_us_east_1" {
+  description = "The ARN of the Lambda function"
+  value       = module.lambda_us_east_1.split_arn
 }
+
+output "merge_arn_us_west_2" {
+  description = "The ARN of the Lambda function"
+  value       = module.lambda_us_west_2.merge_arn
+}
+
+output "merge_arn_us_east_1" {
+  description = "The ARN of the Lambda function"
+  value       = module.lambda_us_east_1.merge_arn
+}
+
+# output "api_gateway_url_us_west_2" {
+#   description = "The URL of the API Gateway"
+#   value       = module.api_gateway_us_west_2.api_gateway_url
+# }
+
+# output "api_gateway_url_us_east_1" {
+#   description = "The URL of the API Gateway"
+#   value       = module.api_gateway_us_east_1.api_gateway_url
+# }
