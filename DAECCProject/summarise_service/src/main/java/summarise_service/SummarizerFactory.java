433cf1a830914d0a2613de821688a57096bc3be7
c7031390 <emanuele.goldin@uibk.ac.at>
2024-01-26 18:48:04 +0100

    Integrated CORE libraries; fixed AWSSummarizer

--- DAECCProject/summarise_service/src/main/java/summarise_service/SummarizerFactory.java
+++ DAECCProject/summarise_service/src/main/java/summarise_service/SummarizerFactory.java
@@ -9,4 +9,38 @@ public class SummarizerFactory {
             default: return null;
         }
     }
+
+    public static SummarizeService Create(FileType fileType){
+        switch (fileType){
+            case PLAIN: return new AWSSummarizer();
+            case PDF: 
+            default: return new GCPSummarizer();
+        }
+    }
+
+    public static SummarizeService Create(String inputUrl){
+        if (inputUrl.startsWith("https://s3.amazonaws.com/") || inputUrl.contains(".s3.amazonaws.com/")) {
+            return new AWSSummarizer();
+        } else if (inputUrl.startsWith("gs://")) {
+            return new GCPSummarizer();
+        } else {
+            // Handle case where URL does not match any known patterns
+            return null;
+        }
+    }
+
+    public static SummarizeService Create(Provider provider, String inputUrl){
+        if (provider != null) {
+            if(!CompliantStorage(provider, inputUrl)){
+                throw new RuntimeException("Trying to access non-compliant storage with provider "+ provider +" and url "+ inputUrl +".");
+            }
+            return Create(provider);
+        } else {
+            return Create(inputUrl);
+        }
+    }
+
+    private static boolean CompliantStorage(Provider provider, String inputUrl){
+        return provider == Provider.AWS && inputUrl.contains("s3.amazonaws.com") || provider == Provider.GCP && inputUrl.contains("gs://");
+    }
 }
