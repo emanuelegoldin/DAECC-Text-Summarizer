364687b68708223c6543a77c4a3e279d4ba1d569
c7031390 <emanuele.goldin@uibk.ac.at>
2024-01-28 00:24:44 +0100

    Add scope to gcp key for oauth2

--- DAECCProject/summarise_service/src/main/java/summarise_service/GCPSummarizer.java
+++ DAECCProject/summarise_service/src/main/java/summarise_service/GCPSummarizer.java
@@ -1,16 +1,19 @@
 package summarise_service;
-import com.google.cloud.documentai.v1beta3.DocumentProcessorServiceSettings;
+import java.io.InputStream;
+import java.util.Collections;
+
 import com.google.api.gax.core.FixedCredentialsProvider;
-import com.google.cloud.documentai.v1beta3.Document;
-import com.google.cloud.documentai.v1beta3.DocumentProcessorServiceClient;
-import com.google.cloud.documentai.v1beta3.ProcessRequest;
-import com.google.cloud.documentai.v1beta3.ProcessResponse;
-import com.google.cloud.documentai.v1beta3.RawDocument;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.documentai.v1.DocumentProcessorServiceSettings;
+import com.google.cloud.documentai.v1.Document;
+import com.google.cloud.documentai.v1.DocumentProcessorServiceClient;
+import com.google.cloud.documentai.v1.ProcessRequest;
+import com.google.cloud.documentai.v1.ProcessResponse;
+import com.google.cloud.documentai.v1.RawDocument;
 import com.google.protobuf.ByteString;
 
 import shared.Credentials;
 
-import java.util.Optional;
 
 // CORE library
 // import shared.Credentials;
@@ -25,17 +28,19 @@ public class GCPSummarizer implements SummarizeService {
 
     public SummarizerResponse processDocument(String content) {
         String endpoint = String.format("%s-documentai.googleapis.com:443", "us");
-        Credentials credentials;
+        GoogleCredentials credentials;
         try {
-            credentials = Credentials.loadDefaultCredentials();
+            InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("key.json");
+            credentials = GoogleCredentials.fromStream(in).createScoped(Collections.singletonList("https://www.googleapis.com/auth/cloud-platform"));;
         } catch (Exception e) {
             throw new RuntimeException("Failed to load credentials: " + e.getMessage());
         }
         DocumentProcessorServiceSettings settings;
+        
         try{
             settings = DocumentProcessorServiceSettings.newBuilder()
                 .setEndpoint(endpoint)
-                .setCredentialsProvider(FixedCredentialsProvider.create((credentials.getGcpCredentials())))
+                .setCredentialsProvider(() -> credentials)
                 .build();
         } catch (Exception e) {
             throw new RuntimeException("Failed to load credentials: " + e.getMessage());
@@ -45,14 +50,15 @@ public class GCPSummarizer implements SummarizeService {
             
             String processorName = System.getenv("PROCESSOR_NAME");
 
-            RawDocument rawDocument = RawDocument.newBuilder()
+            Document rawDocument = Document.newBuilder()
                 .setContent(ByteString.copyFromUtf8(content))
                 .setMimeType("text/plain")
                 .build();
 
             ProcessRequest request = ProcessRequest.newBuilder()
                 .setName(processorName)
-                .setRawDocument(rawDocument)
+                
+                .setInlineDocument(rawDocument)
                 .build();
 
             ProcessResponse response = client.processDocument(request);
