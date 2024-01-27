372e23195247fef20f996805a62bb3f20b4871ba
c7031390 <emanuele.goldin@uibk.ac.at>
2024-01-27 19:31:53 +0100

    fixed runtime error access to method

--- DAECCProject/summarise_service/src/main/java/summarise_service/GCPSummarizer.java
+++ DAECCProject/summarise_service/src/main/java/summarise_service/GCPSummarizer.java
@@ -1,22 +1,16 @@
 package summarise_service;
 import com.google.cloud.documentai.v1beta3.DocumentProcessorServiceSettings;
-import com.google.api.gax.core.CredentialsProvider;
 import com.google.api.gax.core.FixedCredentialsProvider;
-import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.documentai.v1beta3.Document;
 import com.google.cloud.documentai.v1beta3.DocumentProcessorServiceClient;
 import com.google.cloud.documentai.v1beta3.ProcessRequest;
 import com.google.cloud.documentai.v1beta3.ProcessResponse;
 import com.google.cloud.documentai.v1beta3.RawDocument;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.Blob;
-import com.google.cloud.storage.StorageOptions;
 import com.google.protobuf.ByteString;
 
 import shared.Credentials;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.util.Optional;
 
 // CORE library
 // import shared.Credentials;
@@ -25,11 +19,11 @@ import java.net.URISyntaxException;
 
 public class GCPSummarizer implements SummarizeService {
 
-    public SummarizerResponse summarize(String filename) {
-        return processDocument(filename);
+    public SummarizerResponse summarize(String content) {
+        return processDocument(content);
     }
 
-    public SummarizerResponse processDocument(String filePath) {
+    public SummarizerResponse processDocument(String content) {
         String endpoint = String.format("%s-documentai.googleapis.com:443", "us");
         Credentials credentials;
         try {
@@ -39,26 +33,21 @@ public class GCPSummarizer implements SummarizeService {
         }
         DocumentProcessorServiceSettings settings;
         try{
-            GoogleCredentials googleCredentials = credentials.getGcpCredentials();
-            CredentialsProvider credentialsProvider = FixedCredentialsProvider.create(googleCredentials);
-
             settings = DocumentProcessorServiceSettings.newBuilder()
                 .setEndpoint(endpoint)
-                .setCredentialsProvider(credentialsProvider)
+                .setCredentialsProvider(FixedCredentialsProvider.create((credentials.getGcpCredentials())))
                 .build();
         } catch (Exception e) {
-            // TODO: handle exception
-            return null;
+            throw new RuntimeException("Failed to load credentials: " + e.getMessage());
         }
 
         try (DocumentProcessorServiceClient client = DocumentProcessorServiceClient.create(settings)) {
             
             String processorName = System.getenv("PROCESSOR_NAME");
 
-            ByteString content = fetchFileFromStorage(filePath);
             RawDocument rawDocument = RawDocument.newBuilder()
-                .setContent(content)
-                .setMimeType("application/pdf")
+                .setContent(ByteString.copyFromUtf8(content))
+                .setMimeType("text/plain")
                 .build();
 
             ProcessRequest request = ProcessRequest.newBuilder()
@@ -75,18 +64,7 @@ public class GCPSummarizer implements SummarizeService {
             summarizerResponse.summary = summarizedText;  
             return summarizerResponse;
         } catch (Exception e) {
-            // TODO: handle exception
-            return null;
+            throw new RuntimeException("Failed to process document: " + e.getMessage());
         }
     }
-
-    private  ByteString fetchFileFromStorage(String fileLocation) throws URISyntaxException {
-        URI uri = new URI(fileLocation);
-        String bucketName = uri.getHost();
-        String fileName = uri.getPath().substring(1);
-        Storage storage = StorageOptions.getDefaultInstance().getService();
-        Blob blob = storage.get(bucketName, fileName);
-        return ByteString.copyFrom(blob.getContent());
-    }
-
 }
